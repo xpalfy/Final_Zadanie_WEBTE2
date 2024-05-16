581a4d3ef0fba082e2d110c0cd71bc6b476a99d0
juszuf69 <xnyitrai@stuba.sk>
2024-05-16 19:54:47 +0200

    fetch files lost in the dark

---
+++ src/admin/questions/fetchArchiveQuestion.php
@@ -0,0 +1,49 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+require '../../checkType.php';
+require '../../config.php';
+
+check(['0']);
+$conn = getDatabaseConnection();
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $data = json_decode(file_get_contents('php://input'), true);
+    if (isset($data['id']) && is_numeric($data['id'])) {
+        if (isset($data['type']) && $data['type'] === 'One Answer') {
+            $query = "SELECT * FROM answers_archive WHERE question_id = ?";
+        } elseif (isset($data['type']) && $data['type'] === 'Multiple Choice') {
+            $query = "SELECT * FROM abc_answers_archive WHERE question_id = ?";
+        } else {
+            echo json_encode(['success' => false, 'message' => 'Invalid question type.']);
+            exit();
+        }
+        $questionId = $data['id'];
+        $stmt = $conn->prepare($query);
+
+        if ($stmt) {
+            $stmt->bind_param('i', $questionId);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            if ($result->num_rows > 0) {
+                $answers = [];
+                while ($row = $result->fetch_assoc()) {
+                    $answers[] = $row;
+                }
+                echo json_encode(['success' => true, 'questions' => $answers]);
+            } else{
+                echo json_encode(['success' => false, 'message' => 'No answers found for the question.']);
+                exit();
+            }
+        } else{
+            echo json_encode(['success' => false, 'message' => 'Error preparing the select statement.']);
+            exit();
+        }
+    } else{
+        echo json_encode(['success' => false, 'message' => 'Invalid question ID.']);
+        exit();
+    }
+} else{
+    echo json_encode(['success' => false, 'message' => 'Invalid method.']);
+    exit();
+}
