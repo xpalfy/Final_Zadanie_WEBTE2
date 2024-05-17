64785473bf9429d1313b5540cc5f79e8bbc2e6b7
xtukor <xtukor@stuba.sk>
2024-05-17 12:32:48 +0200

    wsserver basic

--- src/wsserver.php
+++ src/wsserver.php
@@ -5,12 +5,16 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 $ws_worker = new Worker("websocket://0.0.0.0:8282");
 
+$ws_worker->count = 4;
 $ws_worker->onConnect = function($connection) {
     echo "New connection\n";
 };
 
-$ws_worker->onMessage = function($connection, $data) {
-    $connection->send('Hello ' . $data);
+$ws_worker->onMessage = function($connection, $data) use ($ws_worker) {
+    // Broadcast data to all connected clients
+    foreach ($ws_worker->connections as $client_connection) {
+        $client_connection->send($data);
+    }
 };
 
 $ws_worker->onClose = function($connection) {
@@ -19,3 +23,4 @@ $ws_worker->onClose = function($connection) {
 
 // Run all workers
 Worker::runAll();
+?>
